810cebacc33058ae4236c8eee516ce493b87a5f6
ribolost <ribolost@hotmail.com>
2026-09-09 04:25:29 -0500

    feat(initial-setup): add initial project structure with application properties and basic pom.xml dependency configuration

---
+++ apps/backend/kataecommerce/src/main/java/com/ribolost/pruebastecnicas/kataecommerce/KataecommerceApplication.java
@@ -0,0 +1,13 @@
+package com.ribolost.pruebastecnicas.kataecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KataecommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KataecommerceApplication.class, args);
+	}
+
+}
